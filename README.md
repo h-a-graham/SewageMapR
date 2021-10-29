15a03d8f5c32e3ffc893ba02b3f08ea4662ae422
Hugh Graham <hugh.a.graham@gmail.com>
2021-10-29 05:08:06 +0100

    Commit the first map and a bunch of data processing...

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # SewageMapR
+
 Some Maps about sewage...
+
+## Data sourecs:
+
+[**England Unmonitored CSOs 2020**](https://data.catchmentbasedapproach.org/datasets/theriverstrust::-england-unmonitored-csos-2020/about)
+
+[**Welsh Unmonitored CSOs 2020**](https://data.catchmentbasedapproach.org/datasets/theriverstrust::welsh-unmonitored-csos-2020/about)
+
+[**Event Duration Monitoring - Storm Overflows - 2020 (England and Wales)**](https://data.catchmentbasedapproach.org/datasets/theriverstrust::event-duration-monitoring-storm-overflows-2020-england-and-wales/about)
+
+## The map:
+
+![BivariateSewage](HowMuchShitAndHowWellDoTheyMeasure.png)
---
+++ bivariate_tmap.R
@@ -0,0 +1,236 @@
+# Header ------------------------------------------------------------------
+# Bivariate choropleth map in R
+# using tmap
+#
+# Bivariate color schemes
+# http://www.joshuastevens.net/cartography/make-a-bivariate-choropleth-map/
+#
+# Author: Stefano De Sabbata
+# Date: 23 November 2018
+
+library(sp)
+# library(spdep)
+library(tmap)
+library(classInt)
+library(grid)
+library(gridExtra)
+library(lattice)
+
+
+# Color scheme ------------------------------------------------------------
+biv_col_pals <- function(biv_palette){
+  if (biv_palette=='BlueOrange') {
+    bvColors <- c("#FFFFFF","#FAB186","#F3742D",
+                  "#97D0E7","#B0988C","#AB5F37",
+                  "#18AEE5","#407B8F","#5C473D")
+  } else if (biv_palette=='BlueRed'){
+    bvColors <- c("#e8e8e8","#e4acac","#c85a5a",
+                  "#b0d5df","#ad9ea5","#985356",
+                  "#64acbe","#627f8c","#574249")
+  } else if (biv_palette=='GreenPurple'){
+    bvColors <- c("#f3f3f3","#c2f0ce","#8ae1ae",
+                  "#eac5dd","#9ec5d3","#7ec5b1",
+                  "#e6a2d0","#bb9fce","#7a8eae")
+  } else stop("I don't have that palette!!!!!")
+
+  return(bvColors)
+}
+
+# Print bivariate map -----------------------------------------------------
+# The function below calls the function that creates the map
+# then adds a square legend and prints the plot
+
+bivariate_choropleth <- function (
+
+    # Function parameters
+    bivmap_dataset,         # A SpatialPoligonDataFrame
+    bivmap_vars,            # A vector of characters containing the name of the two variables
+    bivmap_labels=NA,       # A vector of characters containing the labels for the two variables, to use in the legend
+    bivmap_style='quantile',# Classification type for the bins
+    bivmap_scale=FALSE,      # Use a scale bar
+    basemap=NULL,
+    bm_alpha = 0.5,
+    scale_pos=c("left", "top"),
+    northarrow = TRUE,
+    north_pos=c("right", "top"),
+    poly_alpha=1,
+    biv_palette ='BlueOrange',
+    bivmap_label_point=NULL,
+    bivmap_label_col = NA,
+    footnote = NULL,
+    title=NULL
+  ) {
+
+  bvColors <-biv_col_pals(biv_palette)
+
+  # Create the bivatiate map
+  bivmap <- get_bivariate_choropleth(
+    # Passs parameters on
+    # except labels
+    bivmap_dataset,
+    bivmap_vars,
+    bivmap_style,
+    bivmap_scale,
+    basemap=basemap,
+    bm_alpha = bm_alpha,
+    scale_pos=scale_pos,
+    northarrow = northarrow,
+    north_pos=north_pos,
+    poly_alpha=poly_alpha,
+    bvColors=bvColors,
+    bivmap_label_point=bivmap_label_point,
+    bivmap_label_col = bivmap_label_col,
+    footnote=footnote,
+    title=title
+  )
+
+  if (is.na(bivmap_labels)){
+    bivmap_labels <- bivmap_vars
+  }
+
+  # Print map
+  suppressWarnings(print( bivmap ))
+
+  # Create the square legend
+  vp <- viewport(x=.17, y=.78, width=.3, height=.3, angle = 0) # change angle for diamond but labelling becomes a bit weird...
+  pushViewport(vp)
+  # grid.multipanel(newpage = FALSE)
+  print(levelplot(
+    matrix(1:9, nrow=3),
+    axes=FALSE,
+    col.regions=bvColors,
+    xlab=list(label=bivmap_labels[1],cex=1.5, fontface=3),
+    ylab=list(label=bivmap_labels[2],cex=1.5,fontface=3),
+    cuts=8,
+    colorkey=FALSE,
+    scales=list(draw=0)),
+    newpage=FALSE)
+
+  # Pop viewport
+  popViewport()
+}
+
+
+
+# Create bivariate map ----------------------------------------------------
+# This function actually creates the bivariate map using tmap
+
+get_bivariate_choropleth <- function (
+
+  # Function parameters
+  bivmap_dataset,         # A SpatialPoligonDataFrame
+  bivmap_vars,            # A vector of characters containing the name of the two variables
+  bivmap_style='quantile',# Classification type for the bins
+  bivmap_scale=FALSE ,     # Use a scale bar
+  basemap=NULL,
+  bm_alpha = 0.5,
+  scale_pos=c("left", "top"),
+  northarrow = TRUE,
+  north_pos=c("right", "top"),
+  poly_alpha=1,
+  bvColors='BlueOrange',
+  bivmap_label_point=NULL,
+  bivmap_label_col = NA,
+  footnote=NULL,
+  title=NULL
+) {
+
+  bivmap_dataset <- as(bivmap_dataset,"Spatial")
+  # Extract the two specified colums
+  # excluding rows with na and infinite values
+  #bivmap_sdf <- bivmap_dataset[
+  #  !is.na(bivmap_dataset@data[, bivmap_vars[1]]) &
+  #    !is.na(bivmap_dataset@data[, bivmap_vars[2]]) &
+  #    !is.infinite(bivmap_dataset@data[, bivmap_vars[1]]) &
+  #    !is.infinite(bivmap_dataset@data[, bivmap_vars[2]])
+  #  ,bivmap_vars]
+  bivmap_sdf <- bivmap_dataset[, bivmap_vars]
+
+  # Renaming the variables to simplify the code below
+  colnames(bivmap_sdf@data) <- c("xvar","yvar")
+
+  # Create the 3-class categorization per each variable
+  bivmap_sdf$xcat <- findCols(classIntervals( bivmap_sdf$xvar, n=3, bivmap_style))
+  cat(bivmap_vars[1], "breaks (x-axis):\n")
+  print(classIntervals( bivmap_sdf$xvar, n=3, bivmap_style))
+  #
+  bivmap_sdf$ycat <- findCols(classIntervals( bivmap_sdf$yvar, n=3, bivmap_style))
+  cat(bivmap_vars[2], "breaks (y-axis):\n")
+  print(classIntervals( bivmap_sdf$yvar, n=3, bivmap_style))
+
+  # Combine the above into one 9-class categorization
+  bivmap_sdf$bicat <- bivmap_sdf$xcat + (3 * (bivmap_sdf$ycat - 1))
+
+  bivmap_sdf$bicol <- bvColors[bivmap_sdf$bicat]
+  bivmap_sdf$bicol <- ifelse(is.na(bivmap_sdf$bicol), "#bdbdbd", bivmap_sdf$bicol)
+
+  # Double-check created datasets if necessary
+  #View(bivmap_sdf@data)
+  #View(cbind(bivmap_sdf@data, bivmap_dataset@data))
+
+  # Create the map
+  if (!is.null(basemap)) {
+    bivmap <- tm_shape(basemap, bbox=st_bbox(bivmap_sdf))+
+      tm_rgb(alpha=bm_alpha,legend.show = FALSE)+
+      tm_shape(bivmap_sdf) +
+      # Fill
+      tm_polygons(
+        "bicol", alpha=poly_alpha) +
+      # Remove frame
+      tm_layout(frame=T) +
+      # Add rhe legend
+      tm_legend(scale=0.75)
+
+    if (isTRUE(northarrow)){
+      bivmap <- bivmap + tm_compass(position=north_pos, size=5, text.size=1.4)
+    }
+  } else {
+    bivmap <-
+      tm_shape(bivmap_sdf) +
+      # Fill
+      tm_polygons(
+        "bicol", alpha=poly_alpha) +
+      # Remove frame
+      tm_layout(frame=T) +
+      # Add rhe legend
+      tm_legend(scale=0.75) +
+
+    if (isTRUE(northarrow)){
+      bivmap <- bivmap + tm_compass(position=north_pos, size=7, text.size=2)
+    }
+  }
+
+  if (!is.null(bivmap_label_point)){
+    bivmap <- bivmap +
+      tm_shape(bivmap_label_point)+
+      tm_dots('black', size=0.2, legend.show=F) +
+      tm_text(bivmap_label_col, auto.placement=F, size=1.5, just=c('center'),
+              fontface = 3, ymod=-1)
+  }
+
+
+  if (bivmap_scale) {
+    bivmap <- bivmap  +
+      # Add scale bar
+      tm_scale_bar(
+        position=scale_pos, width=0.25, text.size=1.4)
+  }
+
+  # Return bivariate map
+  if (!is.null(footnote)){
+    bivmap <- bivmap +
+      tm_credits(footnote, position=c("right", "bottom"),
+                 size=0.9, width=0.6)
+  }
+
+  if (!is.null(title)){
+    bivmap <- bivmap +
+      tm_layout(title= title,
+                title.size=2,
+                fontface=2)
+  }
+
+  bivmap
+
+
+}
